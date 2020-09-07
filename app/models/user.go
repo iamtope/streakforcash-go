fe2c03858eb654e114f57285ea9f89b654f8d577
iamtope <adamoabasstope@gmail.com>
2020-09-07 08:11:54 +0100

    set up postgres database connection with go

--- app/models/user.go
+++ app/models/user.go
@@ -1,17 +1,41 @@
 package models
 
-import(
-	"strings"
-	"errors"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-	"github.com/badoux/checkmail"
-    "github.com/jinzhu/gorm"
-    "golang.org/x/crypto/bcrypt"
+	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
 )
-type User struct {
-	gorm.model
-	Email string `gorm:"type:varchar(50);unique_index" json:"email"`
-	Username string `gorm: "type:varchar(20);unique_index" json:"username"`
-	Password string `gorm:"size:100;not null"json:"password"`
-	Role string `gorm:"size:100;not null"json:"password"`
+
+var db *sql.DB
+
+// initialise the databse 
+func init() {
+	e := godotenv.Load() // load .env file, ideally this should be done in the func main
+	if e != nil {
+		fmt.Print(e);
+	}
+
+
+	DBHOST := os.Getenv("DBHOST")
+	DBUSER := os.Getenv("DBUSER")
+	DBPASSWORD := os.Getenv("DBPASSWORD")
+	DBNAME := os.Getenv("DBNAME")
+	DBPORT := os.Getenv("DBNAME")
+
+	dbURL := fmt.Sprintf("DBHOST=%s DBUSER=%s DBPASSWORD=%s DBNAME=%s DBPORT=%s", DBHOST, DBUSER, DBPASSWORD, DBNAME, DBPORT)
+	fmt.Println(dbURL)
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+	panic(err)
+	defer db.Close()
+	} else {
+		fmt.Println("We are connected to the postgresql database", dbURL)
+	}
+
 }
---
+++ app/utils/index.go
@@ -0,0 +1,1 @@
+// build our response format here
